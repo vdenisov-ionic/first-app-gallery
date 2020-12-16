4874299172135438f511e0c6f61a2b2c5a1b373d
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-12-16 16:45:37 +0300

    3-saving-photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -6,7 +6,7 @@ import {
   CameraPhoto,
   CameraSource,
   CameraResultType,
-  FilesystemDirectory, 
+  FilesystemDirectory,
 } from '@capacitor/core'
 
 const { Camera, Filesystem, Storage } = Plugins;
@@ -27,16 +27,51 @@ export class PhotoService {
   public async addNewToGallery() {
     // * take new photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri, 
-      source: CameraSource.Camera, 
-      quality: 100 
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
     });
-  
+
     // * add to array
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
 
+  private async savePicture(cameraPhoto: CameraPhoto): Promise<Photo> {
+    // * generate name and data for file
+    const fileName = new Date().getTime() + '.jpeg';
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // * save file via Filesystem API
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // * return object with metadata
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto): Promise<string> {
+    // * fetch photo, read as blob, convert to base64
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onerror = reject;
+      reader.onload = () => resolve(reader.result as string);
+
+      reader.readAsDataURL(blob);
+    });
+  }
 }
